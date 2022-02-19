a3779e6f7b39291d6c6dd81e4571cdca1b38277d
UmutSutcu <73347487+UmutSutcu@users.noreply.github.com>
2022-02-19 21:43:09 +0300

    Create bobsempire.c

---
+++ bobsempire.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    scanf("%d",&a);
+
+    int i =0;
+
+    while(i<a){
+        int n;
+        int counter=0;
+        scanf("%d",&n);
+
+        int list[4];
+
+        for(int j=0;j<4;j++){
+            scanf("%d",&list[j]);
+        }
+
+        int c[5]={n,0,0,0,0};
+
+        for(int j=0;j<4;j++){
+            while(c[j]>0){
+                if(list[j]>=c[j]){
+                    counter++;
+                    c[j+1]=c[j];
+                    c[j]=0;
+                }
+                else {
+                    counter++;
+                    c[j+1]=list[j];
+                    c[j]=n-list[j];
+                }
+            }
+        }
+        printf("%d\n",counter);
+
+        i++;
+    }
+
+
+    return 0;
+}
